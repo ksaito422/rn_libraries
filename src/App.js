7bf9d762e9326c77151fee0a95ad7f259d3e64ab
saitooooooo <kitsit.39@gmail.com>
2021-03-03 21:06:13 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+export const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>{/* component... */}</SafeAreaView>
+    </>
+  );
+};
