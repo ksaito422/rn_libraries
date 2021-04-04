a9df64a22823df6b80582f727616c9b520712d4a
saito.k <k.saito@impl.co.jp>
2021-04-04 11:44:40 +0900

    ルーティングとスプラッシュスクリーンの設定

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import {RootNavigator} from './Navigation';
+
+export const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+  });
+
+  return <RootNavigator />;
+};
