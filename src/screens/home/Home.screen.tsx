da5d41bafed3ace4fa70c505eb884c799050c91e
saitooooooo <kitsit.39@gmail.com>
2021-04-10 09:54:09 +0900

    HomeScreenのUI作成

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Button } from 'react-native';
+import { SafeAreaView, View, Text } from 'react-native';
+
+import { Avatar } from 'react-native-ui-lib';
+
 import { useNav } from 'src/hooks/useNav';
 import { COLOR, FONT, FLEX } from 'src/styles';
-import { Spacing } from 'src/components/Spacing';
-import { Avatar } from 'react-native-ui-lib';
+
+import { CustomView } from 'src/components/CustomView';
 
 export const HomeScreen = () => {
   const { navigate } = useNav();
@@ -11,10 +14,12 @@ export const HomeScreen = () => {
   return (
     <>
       <SafeAreaView>
-        <View style={FLEX.row}>
-          <Text style={FONT.medium}>ホーム</Text>
-          <Avatar backgroundColor={COLOR.tertiary} label="A" onPress={() => navigate('Profile')} />
-        </View>
+        <CustomView>
+          <View style={[FLEX.row]}>
+            <Text style={FONT.medium}>ホーム</Text>
+            <Avatar backgroundColor={COLOR.tertiary} label="A" onPress={() => navigate('Profile')} />
+          </View>
+        </CustomView>
       </SafeAreaView>
     </>
   );
