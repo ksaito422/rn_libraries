f22bb938c24d9d23a4cbb5ab35a855d0166b7020
saitooooooo <kitsit.39@gmail.com>
2021-04-11 11:31:12 +0900

    no comment

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
 
-import { Avatar } from 'react-native-ui-lib';
+import { Avatar, Button } from 'react-native-ui-lib';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useNav } from 'src/hooks/useNav';
 import { COLOR, FONT, FLEX } from 'src/styles';
@@ -11,6 +12,31 @@ import { CustomView } from 'src/components/CustomView';
 export const HomeScreen = () => {
   const { navigate } = useNav();
 
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('key', value);
+    } catch (e) {
+      console.log('Set error');
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('key');
+      console.log(value);
+    } catch (e) {
+      console.log('Get error');
+    }
+  };
+
+  const clear = async () => {
+    try {
+      await AsyncStorage.clear();
+    } catch (e) {
+      console.log('Clear error');
+    }
+  };
+
   return (
     <>
       <SafeAreaView>
@@ -19,6 +45,27 @@ export const HomeScreen = () => {
             <Text style={FONT.medium}>ホーム</Text>
             <Avatar backgroundColor={COLOR.tertiary} label="A" onPress={() => navigate('Profile')} />
           </View>
+          <Button
+            backgroundColor="blue"
+            label="保存"
+            onPress={() => {
+              storeData('save');
+            }}
+          />
+          <Button
+            backgroundColor="red"
+            label="読み込む"
+            onPress={() => {
+              getData();
+            }}
+          />
+          <Button
+            backgroundColor="yellow"
+            label="storageクリア"
+            onPress={() => {
+              clear();
+            }}
+          />
         </CustomView>
       </SafeAreaView>
     </>
