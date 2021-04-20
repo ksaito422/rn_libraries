e077adbfe20ebb89523658523cec7a293f93f210
saitooooooo <kitsit.39@gmail.com>
2021-04-20 22:32:17 +0900

    グラデーション

--- src/screens/home/Home.screen.tsx
+++ src/screens/home/Home.screen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
 
 import { Avatar } from 'react-native-ui-lib';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { useNav } from 'src/hooks/useNav';
 import { COLOR, FONT, FLEX } from 'src/styles';
@@ -13,14 +14,22 @@ export const HomeScreen = () => {
 
   return (
     <>
-      <SafeAreaView>
-        <CustomView>
-          <View style={[FLEX.row]}>
-            <Text style={FONT.medium}>ホーム</Text>
-            <Avatar backgroundColor={COLOR.tertiary} label="A" onPress={() => navigate('Profile')} />
-          </View>
-        </CustomView>
-      </SafeAreaView>
+      <LinearGradient
+        // colors={['#cfd9df', '#e2ebf0']}
+        // colors={['#f5f7fa', '#c3cfe2']}
+        colors={['#eee', '#ccc', '#aaa']}
+        start={{ x: 0.0, y: 0.0 }}
+        end={{ x: 1, y: 1 }}
+        style={{ flex: 1 }}>
+        <SafeAreaView>
+          <CustomView>
+            <View style={[FLEX.row]}>
+              <Text style={FONT.medium}>ホーム</Text>
+              <Avatar backgroundColor={COLOR.tertiary} label="A" onPress={() => navigate('Profile')} />
+            </View>
+          </CustomView>
+        </SafeAreaView>
+      </LinearGradient>
     </>
   );
 };
